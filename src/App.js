6d9cf160ca346a73e6e8be4607d66f2e152ccafc
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2022-05-27 22:17:28 +0430

    Dictionary component added
    component includes search engine

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <Dictionary />
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
+function Dictionary() {
+  let [searchWord, setSearchWord] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`searching for ${searchWord}`);
+  }
 
-function Dictionary (){
-
-    return (
-        <div> Hello from Dictionary</div>
-    )
+  function updateSearchWord(event) {
+    setSearchWord(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <h1>Dictionary</h1>
+      <h3>What word do you want to look up?</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="search-engine d-flex">
+          <input
+            type="search"
+            placeholder="Enter a word"
+            autoComplete="off"
+            className="form-control"
+            onChange={updateSearchWord}
+          />
+          <button className="btn">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Dictionary;
