289b077fd2fd36344f0d9023a936bf79cc49d88b
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2022-05-29 23:30:36 +0430

    Footer links eddited
    rel="noopener noreferrer" added to links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/Footer.css"
+import "../styles/Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
@@ -7,15 +7,24 @@ export default function Footer() {
       <a
         href="https://www.linkedin.com/in/vida-khalili-bbb500a4/"
         target="_blank"
+        rel="noopener noreferrer"
       >
         Vida Khalili{" "}
       </a>
       and is open-sourced on{" "}
-      <a href="https://github.com/vida-khalili/dictionary-app" target="_blank">
+      <a
+        href="https://github.com/vida-khalili/dictionary-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         GitHub{" "}
       </a>
       and hosted on{" "}
-      <a href="https://github.com/vida-khalili/dictionary-app" target="_blank">
+      <a
+        href="https://magnificent-basbousa-67f14d.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Netlify
       </a>
     </div>
