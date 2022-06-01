f239e47284a789cb1958fe65e25e69455816ead0
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2022-06-01 23:22:21 +0430

    Photos component added
    Pexels API used for the Photos.js component

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Result from "./Result";
+import Photos from "./Photos";
 import "../styles/Dictionary.css";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState("Hello");
   const [loaded, setLoaded] = useState(false);
   const [definition, setDefinition] = useState(null);
+  const [photos, setPhotos] = useState([]);
 
+  function handleImages(response) {
+    setPhotos(response.data.photos);
+  }
 
   function handleResponse(response) {
     setDefinition(response.data[0]);
+    let apiUrl = `https://api.pexels.com/v1/search?query=${response.data[0].word}&per_page=9`;
+    let apiKey = "563492ad6f91700001000001efbee91edbb345cdb9eca061b1f0b0d8";
+    axios
+      .get(apiUrl, { headers: { Authorization: `Bearer ${apiKey}` } })
+      .then(handleImages);
   }
 
   function load() {
@@ -37,8 +47,7 @@ export default function Dictionary(props) {
       <div className="Dictionary">
         <h1>Dictionary</h1>
         <div className="search-card">
-          <h3>What word do you want to look up?
-        </h3>
+          <h3>What word do you want to look up?</h3>
           <form onSubmit={handleSubmit} className="search-engine">
             <input
               type="search"
@@ -48,16 +57,17 @@ export default function Dictionary(props) {
               className="form-control"
               onChange={handleKeywordChange}
             />
-            <button className="btn btn-primary"><i className="fa-solid fa-magnifying-glass" /></button>
+            <button className="btn btn-primary">
+              <i className="fa-solid fa-magnifying-glass" />
+            </button>
           </form>
         </div>
         <Result definition={definition} />
+        <Photos photos={photos} />
       </div>
-  );
-      
+    );
   } else {
     load();
-    return "Loading!"
+    return "Loading!";
   }
-
 }
